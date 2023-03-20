76da11345845d2eddfe225fcacdc414dcd7902e9
Sina Nejadbakhtiari <snejadbakhtiari@gmail.com>
2023-03-20 15:53:03 +0100

    update regsiter-passenger & book-flight

--- Flights/Flights/ClientApp/src/app/register-passenger/register-passenger.component.ts
+++ Flights/Flights/ClientApp/src/app/register-passenger/register-passenger.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PassengerService } from './../api/services/passenger.service';
 import { FormBuilder } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-passenger',
@@ -12,7 +13,8 @@ export class RegisterPassengerComponent implements OnInit {
   constructor(
     private PassengerService: PassengerService,
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   form = this.fb.group({
@@ -26,6 +28,14 @@ export class RegisterPassengerComponent implements OnInit {
 
   checkPassenger(): void {
     const params = { email: this.form.get('email')?.value };
+    this.PassengerService.findPassenger(params as { email: string }).subscribe(
+      this.login,
+      (e) => {
+        if (e.status != 404) {
+          console.error(e);
+        }
+      }
+    );
   }
 
   register() {
@@ -33,10 +43,11 @@ export class RegisterPassengerComponent implements OnInit {
 
     this.PassengerService.registerPassenger({
       body: this.form.value,
-    }).subscribe(
-      (_) =>
-        this.authService.LoginUser({ email: this.form.get('email')?.value }),
-      console.error
-    );
+    }).subscribe(this.login, console.error);
   }
+
+  private login = () => {
+    this.authService.LoginUser({ email: this.form.get('email')?.value });
+    this.router.navigate(['/search-flights']);
+  };
 }
